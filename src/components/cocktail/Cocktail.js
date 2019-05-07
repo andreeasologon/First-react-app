1f3a2153795675482ba8528b6e10bafb9ca132ef
Sologon, Andreea <Andreea.Sologon@centric.eu>
2019-05-07 19:33:56 +0300

    Updated redirect
    Updated redirect

--- src/components/cocktail/Cocktail.js
+++ src/components/cocktail/Cocktail.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './Cocktail.scss';
 export default class Cocktail extends React.Component {
 
-  handleClick = () => {
-   
+  handleClick = (param) => {
+ 
     var data = [{
       idDrink: this.props.idDrink,
       title: this.props.title,
-      imgUrl: this.props.imgUrl
+      imgUrl: this.props.imgUrl,
+      go: param
     }]
     this.props.getDetails(data);
   }
 
   render() {
+ 
     return (
       <div className="tile-container">
-        <p onClick={this.handleClick}>{this.props.title}</p>
-          <img onClick={this.handleClick} src= {this.props.imgUrl} alt={this.props.title}></img>
+        <p onClick={this.handleClick.bind(this, 'title')}>{this.props.title}</p>
+          <img onClick={this.handleClick.bind(this,'img')} src= {this.props.imgUrl} alt={this.props.title}></img>
       </div>
     );
   }
--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -12,17 +12,14 @@ export default class Cocktails extends React.Component {
         idDrink: item.idDrink,
         title: item.title,
         imgUrl: item.imgUrl,
-        isBackTitle: false,
-        isBackImg: false
+        isBack: false,
+        go: item.go
       })
     })
   }
 
   refreshCocktails = (params) => {
-    this.setState({
-      isBackTitle: params,
-      isBackImg: params,
-    })
+    this.setState({ isBack: params })
   }
   render() {
     let cocktailTiles = null;
@@ -35,21 +32,20 @@ export default class Cocktails extends React.Component {
       });
     }
 
-    if (this.state !== null && this.state.isBackTitle === false) {
+    if (this.state !== null && this.state.isBack === false) {
       titleDescription = <TitleDetails key={this.state.idDrink} title={this.state.title} imgUrl={this.state.imgUrl} id={this.state.idDrink} fromTitletoCocktails={this.refreshCocktails} />
     }
 
-    if (this.state !== null && this.state.isBackImg === false) {
-      imgDescription = <ImageDetails key={this.state.idDrink} title={this.state.title} imgUrl={this.state.imgUrl} id={this.state.idDrink} fromImagetoCocktails={this.refreshCocktails} />
+    if(this.state !== null && this.state.isBack === false){
+      imgDescription = <ImageDetails key={this.state.idDrink} title={this.state.title} imgUrl={this.state.imgUrl} id={this.state.idDrink} fromImagetoCocktails={this.refreshCocktails}/>
     }
-
 
     return (
       <div>
-        <h4>{(console.log(this.state))}</h4>
+        <h4>{this.props.category}</h4>
         <div className="tiles-container">
-          {this.state === null ? cocktailTiles : (this.state.isBackTitle ? imgDescription : titleDescription)}
-          {this.state !== null && (this.state.isBackTitle || this.state.isBackImg)? cocktailTiles : ''}
+          {this.state === null ? cocktailTiles : (this.state.go === 'title'? titleDescription: imgDescription) }
+          {this.state !== null && this.state.isBack ? cocktailTiles : ''}
         </div>
       </div >
     );
